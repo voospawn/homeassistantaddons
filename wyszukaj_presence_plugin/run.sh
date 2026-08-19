e1f2ac591b2cfa18b67c289142227832ffb93a40
abk <99986476+voospawn@users.noreply.github.com>
2026-08-19 17:34:46 +0200

    Add files via upload

---
+++ wyszukaj_presence_plugin/run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+# cd /data
+
+while true
+do
+    # echo "========================================"
+    # echo "Starting network scan..."
+    # echo "========================================"
+
+    /app/wyszukaj
+
+    # echo
+    # echo "Scan finished."
+    # echo "Waiting 5 minutes..."
+    # echo
+
+    sleep 300
+done
